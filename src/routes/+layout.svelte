15c365e05fbf2f78086b03884821a9d83a9f93cb
Pritom-Paul <p.paulprem@gmail.com>
2024-08-25 10:25:42 +0600

    merge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,25 @@
-<script>import "../app.css";</script><slot></slot>
+<script lang="ts">
+    import "../app.css";
+    import Navbar from "$lib/Common/Navbar/Navbar.svelte";
+    import { ModeWatcher } from "mode-watcher";
+</script>
+
+<div class="flex flex-col min-h-screen">
+    <!-- Navbar -->
+    <Navbar />
+
+    <!-- Main content area -->
+    <main class="flex-grow">
+        <slot />
+    </main>
+
+    <!-- Footer -->
+    <footer class="w-full p-4 text-center border-t border-opacity-20 border-gray-400 mt-auto">
+        <p class="text-primary-neutral-content dark:text-primary-neutral text-sm">Copyright(c) 2024 - Altersense Limited</p>
+    </footer>
+</div>
+
+<!-- ModeWatcher -->
+<ModeWatcher/>
+
+<slot></slot>
